242e7a573e946ae27240d2c353dae4efa305f43d
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-12-03 18:41:01 +0100

    config project

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('myapp.urls')),
+    path('', include('news.urls')),
 
 ]
 
