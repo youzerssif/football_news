75b269c583752da03c1a9b55bd014976f97428b5
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-12-04 21:22:47 +0100

    add test file for scrap football.fr top article

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import requests
+# -*- coding:utf-8 -*-
 from requests import get
 from bs4 import BeautifulSoup
-
 
 
 def link_actu():
     
     url = "https://www.football.fr/les-tops"
-    
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36'}
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"
+    }
     response = get(url, headers=headers)
     html_soup = BeautifulSoup(response.text, "html.parser")
 
@@ -16,36 +17,57 @@ def link_actu():
 
         # main_div = html_soup.find("div", attrs={"class": "archive-posts row"})
         div_link = html_soup.find_all("div", attrs={"class": "post-item-img"})
-        list_link = [element.find("div")['data-href'] for element in div_link]
-        return list_link     
+        list_link = [element.find("div")["data-href"] for element in div_link]
+        return list_link
     else:
         print("erreur statut", response.status_code)
-        
+
+
 def actu(link):
-        
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36'}
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"
+    }
     response = get(link, headers=headers)
     html_soup = BeautifulSoup(response.text, "html.parser")
 
     if response.status_code == 200:
 
         # actu_article = html_soup.find("article")
-        autor_and_date = html_soup.find("div", attrs={"class": "post-info"}).text.strip().replace('\xa0',' ').replace('\n','')
+        autor_and_date = (
+            html_soup.find("div", attrs={"class": "post-info"})
+            .text.strip()
+            .replace("\xa0", " ")
+            .replace("\n", "")
+        )
         title = html_soup.find("h1", attrs={"class": "post-title"}).text
-        image = html_soup.find("img")['src']
-        short_desc = html_soup.find("div", attrs={"class": "post-excerpt"}).text.strip().replace('\xa0',' ').replace('\n','')
-        description = html_soup.find("div", attrs={"class": "post-content"}).text.strip().replace('\xa0',' ').replace('\n','')
+        image = html_soup.find("img")["src"]
+        short_desc = (
+            html_soup.find("div", attrs={"class": "post-excerpt"})
+            .text.strip()
+            .replace("\xa0", " ")
+            .replace("\n", "")
+        )
+        description = (
+            html_soup.find("div", attrs={"class": "post-content"})
+            .text.strip()
+            .replace("\xa0", " ")
+            .replace("\n", "")
+        )
         article = {
-            "author":autor_and_date,
-            "title":title,
-            "image":image,
-            "short_desc":short_desc,
-            "description":description,
+            "author": autor_and_date,
+            "title": title,
+            "image": image,
+            "short_desc": short_desc,
+            "description": description,
         }
         print(article)
-        # print("ok actu", div_link.text)      
+        return article
+        # print("ok actu", div_link.text)
     else:
         print("erreur statut", response.status_code)
-        
-actu(link_actu()[0])
-        
+
+
+links = link_actu()
+for link in links:
+    actu(link)
