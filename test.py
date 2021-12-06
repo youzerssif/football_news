82608a5952374f6beffd4e566b92326ac1040eef
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-12-06 16:22:57 +0100

    90% finish scrap football.fr

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from requests import get
 from bs4 import BeautifulSoup
 
 
-def link_actu():
+def category():
     
-    url = "https://www.football.fr/les-tops"
+    url = "https://www.football.fr/espagne"
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"
@@ -16,9 +16,55 @@ def link_actu():
     if response.status_code == 200:
 
         # main_div = html_soup.find("div", attrs={"class": "archive-posts row"})
-        div_link = html_soup.find_all("div", attrs={"class": "post-item-img"})
-        list_link = [element.find("div")["data-href"] for element in div_link]
-        return list_link
+        
+        nav_category = html_soup.find("div", attrs={"id": "principal-menu"})
+        li_category = nav_category.find_all("li")
+        list_cat = ["https://www.football.fr/les-tops"]
+        for element in li_category:
+            link_cat = element.find('a')['href']
+            title_cat = element.text.strip()
+            list_cat.append(link_cat)
+            # list_cat = [link_cat, title_cat]
+        # print(list_cat)
+        return list_cat
+        
+    
+    else:
+        print("erreur statut", response.status_code)
+
+
+def link_actu(url):
+    
+    # url = "https://www.football.fr/espagne"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"
+    }
+    response = get(url, headers=headers)
+    html_soup = BeautifulSoup(response.text, "html.parser")
+
+    if response.status_code == 200:
+
+        # main_div = html_soup.find("div", attrs={"class": "archive-posts row"})
+        
+        nav_link = html_soup.find_all("a", attrs={"class": "page-numbers"})[7].text.strip()
+        all_links = []
+        for item in range(1,int(nav_link)+1):
+            url2 = f"{url}/page/{item}"
+            # print(url2)
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"
+            }
+            response = get(url2, headers=headers)
+            html_soup = BeautifulSoup(response.text, "html.parser")
+            
+            div_link = html_soup.find_all("div", attrs={"class": "post-item-img"})
+            list_link = [element.find("div")["data-href"] for element in div_link]
+            all_links.append(list_link)
+        # print(all_links)
+        return all_links
+        
+    
     else:
         print("erreur statut", response.status_code)
 
@@ -42,12 +88,16 @@ def actu(link):
         )
         title = html_soup.find("h1", attrs={"class": "post-title"}).text
         image = html_soup.find("img")["src"]
-        short_desc = (
-            html_soup.find("div", attrs={"class": "post-excerpt"})
-            .text.strip()
-            .replace("\xa0", " ")
-            .replace("\n", "")
-        )
+        try:
+            short_desc = (
+                html_soup.find("div", attrs={"class": "post-excerpt"})
+                .text.strip()
+                .replace("\xa0", " ")
+                .replace("\n", "")
+            )
+        except:
+            short_desc = ""
+            print("Pas de short desc")
         description = (
             html_soup.find("div", attrs={"class": "post-content"})
             .text.strip()
@@ -67,7 +117,9 @@ def actu(link):
     else:
         print("erreur statut", response.status_code)
 
-
-links = link_actu()
-for link in links:
-    actu(link)
+categories_links = category()
+for elt in categories_links:
+    links = link_actu(elt)
+    for item in links:
+        for link in item:
+            actu(link)
